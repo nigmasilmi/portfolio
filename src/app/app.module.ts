bd402754abdbb326143bd508262e16ecef1a1f99
beithaninacafe <beithaninacafe@gmail.com>
2019-10-04 16:35:27 -0300

    initial commit for portfolio development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
