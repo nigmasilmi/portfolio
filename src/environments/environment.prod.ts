fbf7ccd7c7a6f8a79ef59631af6313afe00479b0
nigmasilmi <nigmasilmi.web@gmail.com>
2019-11-05 08:42:07 -0300

    added firebase property to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: 'AIzaSyBlFSZPeU7MrWfPfNQqyOR2ftVTuGTnCfQ',
+    authDomain: 'nigmasilmiportafolio.firebaseapp.com',
+    databaseURL: 'https://nigmasilmiportafolio.firebaseio.com',
+    projectId: 'nigmasilmiportafolio',
+    storageBucket: 'nigmasilmiportafolio.appspot.com',
+    messagingSenderId: '864186140502',
+    appId: '1:864186140502:web:6b75de8c9fc439bb8b38f7',
+    measurementId: 'G-918JQVP3GL'
+  },
 };
